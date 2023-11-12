53528c611a36adccf056af15210ffc88bb8b0fc2
Elizaveta <elizavetash0210@mail.ru>
2023-11-12 15:33:02 +0300

    update

---
+++ WorkWithJSON/Course.swift
@@ -0,0 +1,13 @@
+//
+//  Course.swift
+//  WorkWithJSON
+//
+//  Created by Елизавета Шалдыбина on 12.11.2023.
+//
+
+import Foundation
+
+struct Course: Decodable {
+    let message: String?
+    let status: String?
+}
--- WorkWithJSON/ViewController.swift
+++ WorkWithJSON/ViewController.swift
@@ -7,13 +7,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+enum Link {
+    case myURL
+    
+    var url: URL {
+        switch self {
+        case .myURL:
+            return URL(string: "https://dog.ceo/api/breeds/image/random")!
+        }
     }
-
-
 }
 
+enum Alert {
+    case success
+    case failed
+    
+    var title: String {
+        switch self {
+        case .success:
+            return "Success"
+        case .failed:
+            return "Failed"
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .success:
+            return "You can see the results in the Debug area"
+        case .failed:
+            return "You can see error in the Debug area"
+        }
+    }
+}
+
+final class MainViewController: UIViewController {
+    // MARK: - PrivateMethods
+    private func showAlert(withStatus status: Alert) {
+        let alert = UIAlertController(title: status.title, message: status.message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        DispatchQueue.main.async { [unowned self] in
+            present(alert, animated: true)
+        }
+    }
+    
+    @IBAction func goButtonPushed() {
+        URLSession.shared.dataTask(with: Link.myURL.url) { data, _ , error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            let jsonDecoder = JSONDecoder()
+            do {
+                let course = try jsonDecoder.decode(Course.self, from: data)
+                print(course)
+                self.showAlert(withStatus: .success)
+            } catch let error {
+                print(error.localizedDescription)
+                self.showAlert(withStatus: .failed)
+            }
+        }.resume()
+    }
+}
